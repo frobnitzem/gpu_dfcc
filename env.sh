5040068d057363faa2975a3d3fe741e1b0971fea
David M. Rogers <predictivestatmech@gmail.com>
2015-08-01 22:24:43 -0400

    Add more info to env.sh

--- env.sh
+++ env.sh
@@ -5,3 +5,10 @@ export PATH=$PATH:/shares/rogers/psi4/bin
 export PSIDATADIR=/shares/rogers/psi4/interfaces/share/psi
 export OMP_NUM_THREADS=12
 
+# Calling code:
+# /shares/rogers/psi4public/src/bin/fnocc/df_ccsd.cc
+# Reference residual code:
+# /shares/rogers/psi4public/src/bin/fnocc/df_cc_residual.cc
+# Run cmd:
+# psi4 test/input.dat -n 2
+
